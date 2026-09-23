e74ccb27a82c165c6c8d07b1ab762db700d2d124
ChilukaMounika <mounikanetha577@gmail.com>
2026-09-23 20:03:09 +0530

    changed the url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 //const BASE_URL = "http://localhost:5000/api";
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL  || "/api"; // Use the environment variable for the API URL
-
+//const BASE_URL = import.meta.env.VITE_API_BASE_URL  || "/api"; // Use the environment variable for the API URL
+const BASE_URL = `${import.meta.env.VITE_API_URL || "http://localhost:5000"}/api`;
 async function request(endpoint, options = {}) {
   const url = `${BASE_URL}${endpoint}`;
   const config = {
